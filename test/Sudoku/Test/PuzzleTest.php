ed14c161e963a4c548e805dfbc0c391e0aaa8073
jerend <jvde@netvlies.net>
2012-07-04 13:48:48 +0200

    Add the allegedly hardest sudoku on earth

--- test/Sudoku/Test/PuzzleTest.php
+++ test/Sudoku/Test/PuzzleTest.php
@@ -39,4 +39,38 @@ class CurlTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($sudoku->isSolved());
 
     }
+
+    public function testSolveHard()
+    {
+        // An easy puzzle
+        $sudoku = '
+            800000000
+            003600000
+            070090200
+            050007000
+            000045700
+            000100030
+            001000068
+            008500010
+            090000400';
+
+        // Filter all but digits
+        $sudoku = preg_replace('#[^0-9]*#i', '', $sudoku);
+
+        // Create the rows
+        $sudoku = str_split($sudoku, 9);
+
+        // Create the columns
+        foreach ($sudoku as &$row) {
+            $row = str_split($row);
+        }
+
+        // Create the object
+        $sudoku = new \Sudoku\Puzzle($sudoku);
+        $sudoku->solve();
+
+        // Check if the puzzle is solved
+        $this->assertTrue($sudoku->isSolved());
+
+    }
 }
